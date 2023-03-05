c457e64e219744492ded72b7e613d9a918a1dc97
hadishnr <hadishofinur@gmail.com>
2023-03-05 15:03:35 +0800

    handle error modal

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -8,12 +8,22 @@ const AddUser = props => {
   const [enteredUsername, setEnteredUsername] = useState('');
   const [enteredAge, setEnteredAge] = useState('');
 
+  const [error, setError] = useState();
+
   const addUserHandler = event => {
     event.preventDefault();
     if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+      setError({
+        title: 'Invalid input',
+        message: 'Please enter a valid input',
+      });
       return;
     }
     if (+enteredAge < 1) {
+      setError({
+        title: 'Invalid age',
+        message: 'Please enter a valid age',
+      });
       return;
     }
     props.onAddUser(enteredUsername, enteredAge);
@@ -27,9 +37,19 @@ const AddUser = props => {
   const ageChangeHandler = event => {
     setEnteredAge(event.target.value);
   };
+
+  const errorHandler = () => {
+    setError(null);
+  };
   return (
     <Fragment>
-      <ErrorModal title="An error occured!" message="something went wrong" />
+      {error && (
+        <ErrorModal
+          title={error.title}
+          message={error.message}
+          onConfirm={errorHandler}
+        />
+      )}
       <Card className={classes.input}>
         <form onSubmit={addUserHandler}>
           <label htmlFor="username">Username</label>
